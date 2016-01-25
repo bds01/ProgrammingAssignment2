17be9a171af5d96f7d8a4b157f251237d796fb1a
Rasuri <kishore.rasuri@umww.com>
2016-01-24 20:29:07 -0500

    Submission for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## If there is a value for inverse it will be returned by the 
 
 makeCacheMatrix <- function(x = matrix()) {
  m <- NULL
